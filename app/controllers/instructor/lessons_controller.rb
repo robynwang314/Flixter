de111c57dad67dddde16a9290e6f128effb8c4bb
Robyn Wang <tyrobynwang@gmail.com>
2019-09-30 05:10:37 +0000

    flixter 20

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -21,7 +21,7 @@ class Instructor::LessonsController < ApplicationController
 
   helper_method :current_section 
   def current_section
-      @current_section || = Section.find(params[:section_id])
+      @current_section ||= Section.find(params[:section_id])
   end
 
   def lesson_params
