8656612528e84025aa0a6c8f0f5196296a1b15c9
Robyn Wang <tyrobynwang@gmail.com>
2019-09-30 01:50:24 +0000

    Flixter 17

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
-  end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
+  end 
 end
