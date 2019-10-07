a614dacbf07d551c4a866debee30aa329d29e6eb
Robyn Wang <tyrobynwang@gmail.com>
2019-10-07 22:18:12 +0000

    flixter 29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
+
   end
 
   private 
+
+  def require_authorized_for_current_lesson
+    if  !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Must Be Enrolled to View'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
