22292a4901f0b60e75d2b0afe757ef5b3cc44a83
Robyn Wang <tyrobynwang@gmail.com>
2019-10-09 05:44:34 +0000

    flixter 31

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,7 +1,7 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section, only: [:new, :create]
-  before_action :require_authorized_for_currrent_lesson, only: [:update]
+  before_action :require_authorized_for_current_lesson, only: [:update]
 
   def new
     @lesson = Lesson.new
@@ -20,9 +20,9 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
-  def require_authorized_for_currrent_lesson
+  def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
