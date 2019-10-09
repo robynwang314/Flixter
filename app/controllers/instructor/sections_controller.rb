f3052edb30e869630f15f7791bc7f81e1740bc28
Robyn Wang <tyrobynwang@gmail.com>
2019-10-09 21:59:39 +0000

    flixter 32.21

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -11,7 +12,23 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+ def update
+    current_course.update_attributes(section_params)
+    render plain: 'updated!'
+  end
+
+
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :Unauthorized
+    end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -20,12 +37,16 @@ class Instructor::SectionsController < ApplicationController
   end
 
   helper_method :current_course
-  def current_course 
-    @current_course ||= Course.find(params[:course_id])
+  def current_course
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
   
 end
