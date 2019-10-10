57e4ce115b1d577c1b27e805b3ae5bc1b1b61d29
Robyn Wang <tyrobynwang@gmail.com>
2019-10-10 04:54:42 +0000

    flixter 35

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     end
 
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create]
     end
   end 
 end
